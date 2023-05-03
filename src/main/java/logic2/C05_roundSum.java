1fdfea41f07aeb794d1acb4885541cbcf1c87e8e
Fazıl Demir <fazildemir20@gmail.com>
2023-05-03 18:09:41 +0300

    new solutions added

---
+++ src/main/java/logic2/C05_roundSum.java
@@ -0,0 +1,36 @@
+package logic2;
+
+public class C05_roundSum {
+    /*
+    Question:
+
+    For this problem, we'll round an int value up to the next multiple of 10 if its rightmost digit is 5 or more,
+    so 15 rounds up to 20. Alternately, round down to the previous multiple of 10 if its rightmost digit is less than 5,
+    so 12 rounds down to 10. Given 3 ints, a b c, return the sum of their rounded values.
+    To avoid code repetition, write a separate helper "public int round10(int num)
+    {" and call it 3 times. Write the helper entirely below and at the same indent level as roundSum().
+
+    roundSum(16, 17, 18) → 60
+    roundSum(12, 13, 14) → 30
+    roundSum(6, 4, 4) → 10
+    */
+    public static void main(String[] args) {
+        System.out.println(roundSum(12, 13, 14));
+    }
+    public static int roundSum(int a, int b, int c) {
+
+        return round10(a)+round10(b)+round10(c);
+
+    }
+    public static int round10(int num) {
+        int kalan=num%10;
+        if(kalan>=5){
+            return num+=(10-kalan);
+        }else if(kalan<5){
+            return num=(num-kalan);
+        }
+
+        return num;
+    }
+
+}
