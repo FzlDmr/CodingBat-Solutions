3f52d48acf364f72aeb29c8d712e486780af946b
Fazıl Demir <fazildemir20@gmail.com>
2023-05-06 20:29:46 +0300

    new solutions added

---
+++ src/main/java/array2/C25_shiftLeft.java
@@ -0,0 +1,37 @@
+package array2;
+
+import java.util.Arrays;
+
+public class C25_shiftLeft {
+    /*
+    Question:
+
+    Return an array that is "left shifted" by one -- so {6, 2, 5, 3} returns {2, 5, 3, 6}.
+    You may modify and return the given array, or return a new array.
+
+    shiftLeft([6, 2, 5, 3]) → [2, 5, 3, 6]
+    shiftLeft([1, 2]) → [2, 1]
+    shiftLeft([1]) → [1]
+    */
+    public static void main(String[] args) {
+        int [] arr ={6, 2, 5, 3};
+        System.out.println(Arrays.toString(shiftLeft(arr)));
+    }
+    public static int[] shiftLeft(int[] nums) {
+
+        if(nums.length<1){
+            return nums;
+        }
+
+        int[]arr=new int[nums.length];
+        int a=nums[0];
+
+        for(int i=1;i<arr.length;i++){
+            arr[i-1]=nums[i];
+        }
+        arr[arr.length-1]=a;
+
+        return arr;
+    }
+
+}
