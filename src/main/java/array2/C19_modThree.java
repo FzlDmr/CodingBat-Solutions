aa71089d8670a7d8fd1098d2e5a6fae520c14527
Fazıl Demir <fazildemir20@gmail.com>
2023-06-15 20:48:29 +0300

    new solutions added

--- src/main/java/array2/C19_modThree.java
+++ src/main/java/array2/C19_modThree.java
@@ -1,4 +1,21 @@
 package array2;
 
 public class C19_modThree {
+    /*
+    Given an array of ints, return true if the array contains either 3 even or 3 odd values all next to each other.
+
+    modThree([2, 1, 3, 5]) → true
+    modThree([2, 1, 2, 5]) → false
+    modThree([2, 4, 2, 5]) → true
+     */
+
+    public boolean modThree(int[] nums) {
+        for (int i = 0; i <= nums.length - 3; i++) {
+            if (nums[i] % 2 == nums[i + 1] % 2 && nums[i] % 2 == nums[i + 2] % 2) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
